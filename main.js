315acbff2002395d48404a0c16c510e6f8fcf2d6
Eugeniovinicio Berretta <euberdeveloper@gmail.com>
2019-03-16 16:45:39 +0100

    fix main master

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.all('/', (req, res) => {
-    res.send('CIAO');
-});
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 app.listen(PORT, () => {
     console.log('PORT ', PORT);
