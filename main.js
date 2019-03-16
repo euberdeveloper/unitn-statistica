3b884e8b7ce33fdffbb8ef56545a7ff311b2b985
Eugeniovinicio Berretta <euberdeveloper@gmail.com>
2019-03-16 16:00:24 +0100

    try to fix port

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 8000;
+
 app.all('/', (req, res) => {
     res.send('CIAO');
 });
 
-app.listen(80, () => {
+app.listen(PORT, () => {
+    console.log('PORT ', PORT);
     console.log('SERVER LISTENING');
 });
