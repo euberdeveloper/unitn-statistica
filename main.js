47f0e9080271afa52599dfdc9da59ca1342f7dfa
Eugeniovinicio Berretta <euberdeveloper@gmail.com>
2019-03-16 15:52:25 +0100

    added mock response

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.all('/', (req, res) => {
+    res.send('CIAO');
+});
+
 app.listen(80, () => {
     console.log('SERVER LISTENING');
 });
