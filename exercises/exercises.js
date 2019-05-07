b8bc6933bd0018ee501d913348a61fcbc4be1ef7
euberdeveloper <33126163+euberdeveloper@users.noreply.github.com>
2019-05-07 20:37:31 +0200

    notes: 07-06-2019

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -932,7 +932,7 @@ module.exports = [
 
     {
         date: '2019-05-07',
-        notes: 'Mettere almeno 10 milioni come numero di test',
+        notes: 'Mettere almeno 10 milioni come numero di test. Terzo controverso, guardare sul gruppo telegram',
         inputs: [
             {
                 description: 'Scrivere lambda',
