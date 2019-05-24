06b215d80a57d950ad023561558ee5a06231ac3f
Eugeniovinicio Berretta <euberdeveloper@gmail.com>
2019-05-24 21:25:05 +0200

    added easter egg: 23-05-2019

--- exercises/2019-05-23.js
+++ exercises/2019-05-23.js
@@ -4,6 +4,40 @@ class Cerchio {
         return (2 * Math.sqrt(1 - (x ** 2))) / Math.PI;
     }
 
+    _boolLanguage(bool) {
+        const n = Math.floor(Math.random() * 14) + 1; 
+        switch(n) {
+            case 1: 
+                return (bool ? 'VERO' : 'FALSO');
+            case 2:
+                return (bool ? 'TRUE' : 'FALSE');
+            case 3:
+                return (bool ? 'RICHTICH' : 'FALSCH');
+            case 4:
+                return (bool ? 'VERDAD' : 'FALSO');
+            case 5:
+                return (bool ? '实' : '假');
+            case 6:
+                return (bool ? 'リアル' : '偽');
+            case 7:
+                return (bool ? '현실' : '그릇된');
+            case 8:
+                return (bool ? 'իրական' : 'կեղծ'); 
+            case 9:
+                return (bool ? 'حقيقي' : 'زائف');
+            case 10:
+                return (bool ? 'אמיתי' : 'שקר');
+            case 11:
+                return (bool ? 'असली' : 'झूठा');    
+            case 12:
+                return (bool ? 'реальный' : 'фальшивый');    
+            case 13:
+                return (bool ? 'πραγματικός' : 'ψευδής');   
+            case 14:
+                return (bool ? 'maoli' : 'wahahee');   
+        }
+    }
+
     constructor(first, second) {
         this.first = +first;
         this.second = +second;
@@ -18,11 +52,11 @@ class Cerchio {
     }
 
     _solveThird() {
-        return 'FALSE';
+        return this._boolLanguage(false);
     }
 
     _solveFourth() {
-        return 'TRUE';
+        return this._boolLanguage(true);
     }
 
     async test(times, callback) {
