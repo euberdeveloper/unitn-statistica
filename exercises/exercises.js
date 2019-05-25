4b40d2e098214e356e76434b180290455a9b2cec
euberdeveloper <33126163+euberdeveloper@users.noreply.github.com>
2019-05-25 13:34:39 +0200

    Removed notes: 24-05-2019

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1379,7 +1379,6 @@ module.exports = [
 
     {
         date: '2019-05-24',
-        notes: 'Non ancora sicuro al 100%',
         inputs: [
             {
                 description: 'Scrivere i dati separati da virgola',
